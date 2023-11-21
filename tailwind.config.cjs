2391c9c4025b786e936780211acc5fe421493bbf
Juan-Gines <JuangAlenta@hotmail.com>
2023-11-21 20:31:34 +0100

    fix bg image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,40 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		fontFamily: {
-			sans: ['Roboto', 'sans-serif'],
-			dancing: ['Dancing Script', 'cursive']			
-		},
-		extend: {
-			animation: {
-				aparecer: 'aparecer 1.5s linear forwards',
-				growDown: 'growDown 300ms ease-in-out forwards',
-				growUp: 'growUp 300ms ease-in-out forwards',
-			},
-			backgroundImage: {
-        'header': "url('src/assets/img/bgheader.png')",        
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      dancing: ['Dancing Script', 'cursive']
+    },
+    extend: {
+      animation: {
+        aparecer: 'aparecer 1.5s linear forwards',
+        growDown: 'growDown 300ms ease-in-out forwards',
+        growUp: 'growUp 300ms ease-in-out forwards'
       },
-			keyframes: {
-				aparecer: {
-					'0%': { opacity: '0'},
-					'100%': { opacity: '1' },
-				},
-				growDown: {
-					'0%': { transform: 'scaleY(0)' },
-					'80%': { transform: 'scaleY(1.1)' },
-					'100%': { transform: 'scaleY(1)' },
-				},
-				growUp: {
-					'0%': { transform: 'scaleY(1)' },
-					'20%': { transform: 'scaleY(1.1)' },
-					'100%': { transform: 'scaleY(0)' },
-				},
-			},
-			transformOrigin: {
-				'top-center': 'top center',
-			}
-		},
-	},
-	plugins: [],
-};
+      keyframes: {
+        aparecer: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' }
+        },
+        growDown: {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.1)' },
+          '100%': { transform: 'scaleY(1)' }
+        },
+        growUp: {
+          '0%': { transform: 'scaleY(1)' },
+          '20%': { transform: 'scaleY(1.1)' },
+          '100%': { transform: 'scaleY(0)' }
+        }
+      },
+      transformOrigin: {
+        'top-center': 'top center'
+      }
+    }
+  },
+  plugins: []
+}

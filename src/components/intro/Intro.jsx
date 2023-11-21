f6273f4f73704e19ce62f2b613f6fc3dfccb542c
Juan-Gines <JuangAlenta@hotmail.com>
2023-11-21 22:08:06 +0100

    primer commit juangi

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -29,13 +29,13 @@ const Intro = () => {
           {page.title}
         </h1>
       </div>
-      <div className='absolute top-1/4 w-3/4  text-xl sm:text-lg font-sans font-family: text-blue-400 shadow-gray-900'>
+      <div className='absolute top-1/4 w-1/2  text-xl sm:text-lg font-sans font-family: text-blue-400 shadow-gray-900'>
         <AnimateString
           str={msg}
           str2={msg2}
         />
       </div>
-      <div className='absolute bottom-0 h-1/5 w-full flex justify-center items-center'>
+      <div className='absolute bottom-6 h-1/5 w-full flex justify-center items-center'>
         <Link to='/home'>
           <button className='px-6 py-2 text-white text-xl bg-blue-500 rounded-full active:bg-blue-800 hover:bg-blue-400 border border-blue-500'>
             Entrar

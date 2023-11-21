66be10acc6d317587b4ef0d3ad6a8693526f8e17
Juan-Gines <JuangAlenta@hotmail.com>
2023-11-21 20:34:37 +0100

    Nuevo fix bg image

--- src/components/sections/header/MainHeader.jsx
+++ src/components/sections/header/MainHeader.jsx
@@ -5,7 +5,7 @@ const MainHeader = () => {
   const { ui: { page: { icon, title } } } = useContext(UiContext)
 
   return (
-    <header className='bg-local bg-cover flex justify-center' style='backgroundImage: url(src/assets/img/bgheader.png)'>
+    <header className='bg-cover flex justify-center' style='background-image: url(src/assets/img/bgheader.png)'>
       <div className='flex gap-2 md:gap-4 mt-10 items-center py-20'>
         <div className='w-8 sm:w-10 md:w-14'>
           <img
@@ -13,7 +13,7 @@ const MainHeader = () => {
             alt={`Imagen ${title}`}
           />
         </div>
-        <h1 className='text-4xl sm:text-5xl md:text-7xl font-dancing text-stone-800 font-bold'>
+        <h1 className='text-4xl sm:text-5xl md:text-7xl font-dancing text-white font-bold'>
           {title}
         </h1>
       </div>
